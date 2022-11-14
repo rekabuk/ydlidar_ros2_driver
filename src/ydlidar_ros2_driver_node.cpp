35f49d4de9084234dc96e8c27f9787efbefebf2c
Andrew <andrew@rekabuk.co.uk>
2022-11-14 14:14:45 +0000

    Corrected bool type and uart port

--- src/ydlidar_ros2_driver_node.cpp
+++ src/ydlidar_ros2_driver_node.cpp
@@ -146,7 +146,7 @@ int main(int argc, char *argv[]) {
   laser.setlidaropt(LidarPropScanFrequency, &f_optvalue, sizeof(float));
 
   bool invalid_range_is_inf = false;
-  node->declare_parameter<float>("invalid_range_is_inf");
+  node->declare_parameter<bool>("invalid_range_is_inf");
   node->get_parameter("invalid_range_is_inf", invalid_range_is_inf);
 
 
